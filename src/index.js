e09d1a37963584d2fa210450c5bb12f18bff1da2
Brian Mearns <bmearns@commercehub.com>
2018-07-05 22:33:39 -0400

    Add Remaining Params to API and Add Verbose

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 function getRotation (memberCount, options) {
-  const modulus = memberCount - 1
-  const elapsedTime = options.date - options.epoch
-  const intervalNum = parseInt(options.offset + (elapsedTime / options.period))
-  const rotation = ((intervalNum % modulus) + modulus) % modulus // handle negative offsets.
-  return 1 + rotation
+
 }
 
 function generatePairs (members, options) {
-  const rotation = getRotation(members.length, options)
-  return members.map((member, idx) => {
+  const modulus = members.length - 1
+  const elapsedTime = options.date - options.epoch
+  const intervalNum = parseInt(options.offset + (elapsedTime / options.period))
+  const baseRotation = ((intervalNum % modulus) + modulus) % modulus // handle negative offsets.
+  const rotation = 1 + baseRotation
+  const pairs = members.map((member, idx) => {
     return [member, members[(rotation + idx) % members.length]]
   })
+  if (options.verbose) {
+    return {
+      pairs,
+      rotation,
+      elapsedTime: {
+        ms: elapsedTime
+      },
+      period: {
+        ms: options.period
+      },
+      interval: intervalNum,
+      date: options.date,
+      epoch: options.epoch,
+      offset: options.offset
+    }
+  } else {
+    return pairs
+  }
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -37,10 +37,23 @@ function configureApiRouter (router) {
   router.get('/pairs', (request, response) => {
     try {
       const members = parseMembers(request.query.members)
-      const argv = [
-        ...members
-      ]
-      const args = parseArgs(argv)
+      const argv = []
+      if (request.query.period) {
+        argv.push('--period', request.query.period)
+      }
+      if (request.query.offset) {
+        argv.push('--offset', request.query.offset)
+      }
+      if (request.query.epoch) {
+        argv.push('--epoch', request.query.epoch)
+      }
+      if (request.query.date) {
+        argv.push('--date', request.query.date)
+      }
+      const args = parseArgs([...argv, '--', ...members])
+      if (request.query.verbose && request.query.verbose.toLowerCase() == 'true') {
+        args.verbose = true
+      }
       response.json(generatePairs(args.members, args))
     } catch (e) {
       response.status(StatusCodes.BAD_REQUEST).json({error: e.message})
